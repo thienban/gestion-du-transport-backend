6828df40aa942c72d1fbc7b825ccf4f4aa7d24ce
thien ban BUI <thienban91@hotmail.fr>
2018-01-19 17:51:36 +0100

    get reservation

--- src/main/java/controller/ReservCovoiturageController.java
+++ src/main/java/controller/ReservCovoiturageController.java
@@ -1,11 +1,13 @@
 package controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import entite.ReservCovoiturage;
 import repository.ReservCovoiturageRepository;
@@ -24,8 +26,10 @@ public class ReservCovoiturageController {
 	 */
 	
 	@RequestMapping(method = RequestMethod.GET, path = "/creer")
-	public List<ReservCovoiturage> listerReservation (){
-		return reRepo.findAll();
+	public List<ReservCovoiturage> listerReservation (@RequestParam("AdrDep") String AdresseDepart,
+			@RequestParam("AdrDes") String AdresseDestination,
+			@RequestParam("DateDep") LocalDateTime DateDepart ){
+		return reRepo.findByAdresseDepartAndAdresseArriveeAndDateDepart(AdresseDepart, AdresseDestination, DateDepart);
 		
 	}
 }
--- src/main/java/repository/ReservCovoiturageRepository.java
+++ src/main/java/repository/ReservCovoiturageRepository.java
@@ -1,9 +1,11 @@
 package repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import entite.ReservCovoiturage;
 
 public interface ReservCovoiturageRepository extends JpaRepository<ReservCovoiturage, Integer>{
-
+	List<Annonce> findByAdresseDepartAndAdresseArriveeAndDateDepart (String AdresseDepart, String AdresseArivee, LocalDateTime DateDepart);
 }
