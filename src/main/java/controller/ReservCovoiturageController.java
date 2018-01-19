bd6d6ce89e9aa3b7786b6721112525ee229a147c
thien ban BUI <thienban91@hotmail.fr>
2018-01-19 14:19:38 +0100

    reservation controller

--- src/main/java/controller/ReservCovoiturageController.java
+++ src/main/java/controller/ReservCovoiturageController.java
@@ -1,15 +1,31 @@
 package controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import entite.ReservCovoiturage;
+import repository.ReservCovoiturageRepository;
 
 @Controller
 @RequestMapping("/collaborateur/reservations/")
 public class ReservCovoiturageController {
+	
+	@Autowired
+	private ReservCovoiturageRepository reRepo;
 
 	/*
 	 * @GetMapping public List<ReservCovoiturage> ListeReservCovoiturage(){
 	 * 
 	 * return ; }
 	 */
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/creer")
+	public List<ReservCovoiturage> listerReservation (){
+		return reRepo.findAll();
+		
+	}
 }
