c83d7c6321d895cbf4d8d1c5f6ff18402ac94ec2
thien ban BUI <thienban91@hotmail.fr>
2018-01-22 09:51:45 +0100

    clean

--- src/main/java/controller/ReservCovoiturageController.java
+++ src/main/java/controller/ReservCovoiturageController.java
@@ -6,30 +6,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import entite.ReservCovoiturage;
-import repository.ReservCovoiturageRepository;
 
 @Controller
 @RequestMapping("/collaborateur/reservations/")
 public class ReservCovoiturageController {
 	
-	@Autowired
-	private ReservCovoiturageRepository reRepo;
 
 	/*
 	 * @GetMapping public List<ReservCovoiturage> ListeReservCovoiturage(){
 	 * 
 	 * return ; }
 	 */
-	
-	@RequestMapping(method = RequestMethod.GET, path = "/creer")
-	public List<ReservCovoiturage> listerReservation (@RequestParam("AdrDep") String AdresseDepart,
-			@RequestParam("AdrDes") String AdresseDestination,
-			@RequestParam("DateDep") LocalDateTime DateDepart ){
-		return reRepo.findByAdresseDepartAndAdresseArriveeAndDateDepart(AdresseDepart, AdresseDestination, DateDepart);
-		
-	}
 }
--- src/main/java/repository/MarqueRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import entite.Marque;
-
-public interface MarqueRepository extends JpaRepository<Marque, Integer>{
-
-}
