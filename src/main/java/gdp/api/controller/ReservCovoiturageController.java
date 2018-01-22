f4e30c87f1ab58ac537fdd998ef7088e7cb05777
thien ban BUI <thienban91@hotmail.fr>
2018-01-22 18:39:00 +0100

    ???

--- src/main/java/gdp/api/controller/ReservCovoiturageController.java
+++ src/main/java/gdp/api/controller/ReservCovoiturageController.java
@@ -1,15 +1,15 @@
 package gdp.api.controller;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,7 +33,6 @@ public class ReservCovoiturageController {
 		return annonceRepo.findAll();
 	}
 
-
 	@GetMapping(path = "/me")
 	public List<Annonce> MesReservations() {
 		String email = SecurityContextHolder.getContext().getAuthentication().getName();
@@ -44,10 +43,11 @@ public class ReservCovoiturageController {
 	}
 
 	@PostMapping(path = "/creer")
-	public Annonce CreerReservations(@Param("annonce_id") Integer annonce_id) {
+	public Annonce CreerReservations(@RequestBody Map<String, Integer> body) {
+		Integer annonce_id = body.get("annonce_id");
 		String email = SecurityContextHolder.getContext().getAuthentication().getName();
 		Collaborateur collab = collabRepo.findByEmail(email);
-		Annonce annonce = annonceRepo.getOne(annonce_id);
+		Annonce annonce = annonceRepo.findOne(annonce_id);
 		if (annonce.getNbPlacesRestantes() > 0) {
 			List<Collaborateur> passagers = annonce.getPassagers();
 			passagers.add(collab);
