9dd04923ddc9c737029d96f97e6602bb8ee51e72
thien ban BUI <thienban91@hotmail.fr>
2018-01-22 16:12:30 +0100

    enlever les imports inutiles

--- src/main/java/gdp/api/controller/ReservCovoiturageController.java
+++ src/main/java/gdp/api/controller/ReservCovoiturageController.java
@@ -5,8 +5,6 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/gdp/api/repository/AnnonceRepository.java
+++ src/main/java/gdp/api/repository/AnnonceRepository.java
@@ -1,7 +1,5 @@
 package gdp.api.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import gdp.api.entities.Annonce;
