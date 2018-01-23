93cbf22f2683c036afad2d8a840d7ec9174cfea0
AlexGeb <alex.behaghel@gmail.com>
2018-01-23 11:02:22 +0100

    renaame autocomplete controller to MapsController

--- src/main/java/gdp/api/controller/MapsController.java
+++ src/main/java/gdp/api/controller/MapsController.java
@@ -1,5 +1,6 @@
 package gdp.api.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,24 +10,32 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.maps.errors.ApiException;
 
 import gdp.api.entities.Annonce;
 import gdp.api.entities.Collaborateur;
+import gdp.api.entities.TrajetInfo;
 import gdp.api.repository.AnnonceRepository;
 import gdp.api.repository.CollaborateurRepository;
 import gdp.api.services.GoogleApiService;
 
 @RestController
-@RequestMapping("autocomplete")
-public class AutocompletionController {
+@RequestMapping("maps")
+public class MapsController {
 	@Autowired GoogleApiService googleApiSvc;
 
 
-	@GetMapping(path = "/{input}")
+	@GetMapping(path = "/autocomplete/{input}")
 	public List<String> getPredictions(@PathVariable String input) {
 		return googleApiSvc.autocompleteAdress(input);
 	}
 
-
+	@GetMapping(path = "/directions")
+	public TrajetInfo getTrajetInfo(@RequestParam("origin") String origin,@RequestParam("destination") String destination) throws ApiException, InterruptedException, IOException {
+		return googleApiSvc.getTrajetInfo(origin, destination);
+	}
+	
 }
