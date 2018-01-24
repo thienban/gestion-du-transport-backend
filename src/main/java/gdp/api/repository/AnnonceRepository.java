7e6ed024586b8245e258b2f8313466ec6705f3ba
AlexGeb <alex.behaghel@gmail.com>
2018-01-24 17:58:53 +0100

    recherche des annonces disponibles ok

--- src/main/java/gdp/api/repository/AnnonceRepository.java
+++ src/main/java/gdp/api/repository/AnnonceRepository.java
@@ -1,5 +1,6 @@
 package gdp.api.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,7 +11,15 @@ import gdp.api.entities.Collaborateur;
 
 public interface AnnonceRepository extends JpaRepository<Annonce, Integer> {
 	public List<Annonce> findByAuteur(Collaborateur collab);
-	
-	@Query(value="SELECT * FROM (SELECT adresse_depart AS adresse FROM annonce UNION SELECT adresse_arrive FROM annonce) WHERE LOWER(adresse) LIKE LOWER('%' || ?1 || '%')",nativeQuery=true)
+
+	@Query(value = "SELECT * FROM (SELECT adresse_depart AS adresse FROM annonce UNION SELECT adresse_arrive FROM annonce) WHERE LOWER(adresse) LIKE LOWER('%' || ?1 || '%')", nativeQuery = true)
 	public List<String> selectAllAdresses(String input);
+
+	@Query("SELECT a FROM Annonce a WHERE ?1 NOT IN a.passagers AND ?1 != a.auteur AND a.dateDepart < CURRENT_TIMESTAMP")
+	List<Annonce> findAvailableReservations(Collaborateur pour);
+
+	List<Annonce> findByDateDepartGreaterThan(LocalDateTime date);
+
+	List<Annonce> findByDateDepartGreaterThanAndAuteurIsNot(LocalDateTime date, Collaborateur notauteur);
+
 }
